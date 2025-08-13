a3f20af04b6211cad28f59cb8d6780978284df77
Jasper Bailey <jasperbailey98@gmail.com>
2025-08-13 17:57:09 +0100

    check all form elements are visible

--- tests/interest-calc.spec.ts
+++ tests/interest-calc.spec.ts
@@ -8,4 +8,14 @@ test.describe("Interest Calculator Tests", () => {
 			page.locator('a[title="Manage"][href="/Account/Manage"]')
 		).toHaveText("Hello jasperbailey98@gmail.com!"); //TODO .env this
 	});
+
+	test("should display all required form elements", async ({ page }) => {
+		await page.goto("/");
+
+		await expect(page.locator(".custom-range")).toBeVisible();
+		await expect(page.locator("#dropdownMenuButton")).toBeVisible();
+		await expect(page.locator("#durationList")).toBeVisible();
+		await expect(page.locator("#gridCheck1")).toBeVisible();
+		await expect(page.locator("button.btn-primary")).toBeVisible();
+	});
 });
